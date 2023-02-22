5216c11e338c583f3431aaa00e545cbfe42909a1
Patrick Roza <contact@patrickroza.com>
2023-02-22 18:39:03 +0100

    Update file2.ts

--- file2.ts
+++ file2.ts
@@ -4,7 +4,6 @@ import { makeClass, Test1 } from "./file1"
 export class Test3 extends Test1 { }
 
 // bad
-// 'extends' clause of exported class 'Test2' has or is using private name ''
-// 'extends' clause of exported class 'Test2' has or is using private name 'Hash'
+// 'extends' clause of exported class 'Test2' has or is using private name 's'
 export class Test2 extends makeClass() {
 }
